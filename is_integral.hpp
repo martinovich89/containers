b7e70f42e85a48189b617b64c2a851270793c8e1
mhenry <mhenry@student.42.fr>
2022-12-14 18:50:13 +0100

    added enable_if.hpp and is_integral_new.hpp

--- is_integral.hpp
+++ is_integral.hpp
@@ -1,5 +1,3 @@
-#pragma once
-
 namespace ft
 {
 	struct true_type {};
---
+++ is_integral_new.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "enable_if.hpp"
+#include "is_integral.hpp"
+
+namespace ft
+{
+	struct T
+	{
+		enum { int_t, float_t } type;
+		template< typename Integer, enable_if< is_integral< Integer >::value, bool > = true >
+    	T(Integer) : type(int_t)
+		{
+
+		}
+	}
+};
+}
